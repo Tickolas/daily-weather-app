9bef2d835afd2bbe3b5bb1f0d30c4bd296daa8e4
Markus Averstad <markus.averstad@folksam.se>
2020-03-22 17:21:48 +0100

    Cleanup and fix test.

--- src/util/chart.util.js
+++ src/util/chart.util.js
@@ -3,7 +3,7 @@ import moment from 'moment';
 export function formatForChart(weatherEntries) {
     const formatted = {};
 
-    weatherEntries.forEach(entry => (formatted[moment(entry.time).format('HH:mm')] = `${entry.temperature}°C`));
+    weatherEntries.forEach(entry => (formatted[moment(entry.time).format('HH:mm')] = entry.temperature));
 
     return formatted;
 }
